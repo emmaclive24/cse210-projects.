ee4158f62718b1690af197e786f89888d5c78179
Clive Tumbare <131152728+emmaclive24@users.noreply.github.com>
2023-10-31 12:05:27 +0200

    Update Program.cs

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,20 @@
-using System;
-
-class Program
+static void Main(string[] args)
 {
-    static void Main(string[] args)
+    List<Shape> shapes = new List<Shape>();
+
+    Square square = new Square("Red", 5);
+    shapes.Add(square);
+
+    Rectangle rectangle = new Rectangle("Blue", 4, 6);
+    shapes.Add(rectangle);
+
+    Circle circle = new Circle("Green", 3);
+    shapes.Add(circle);
+
+    foreach (Shape shape in shapes)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.WriteLine($"Color: {shape.Color}");
+        Console.WriteLine($"Area: {shape.GetArea()}");
+        Console.WriteLine();
     }
 }
