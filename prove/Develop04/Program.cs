7425688c7ddbc57d7c82806231e9fb4be80c00a4
Clive Tumbare <131152728+emmaclive24@users.noreply.github.com>
2023-11-04 13:39:53 +0200

    Update Program.cs
    fixed the program.cs file

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -12,9 +12,9 @@ class Program
         Console.Clear();
 
         Console.WriteLine("Menu Options");
-        Console.WriteLine("1. Start breathing activity");
-        Console.WriteLine("2. Start reflecting activity");
-        Console.WriteLine("3. Start listing activity");
+        Console.WriteLine("1. Start  the breathing activity");
+        Console.WriteLine("2. Start  the reflecting activity");
+        Console.WriteLine("3. Start the listing activity");
         Console.WriteLine("4. Quit");
         Console.Write("Select a choice from the menu (enter a number):  ");
         userInput = Console.ReadLine();
